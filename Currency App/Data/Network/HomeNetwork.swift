1938980806211f9dcb638762a8885963412dea90
Mohamed Khaled <mohammdk393@gmail.com>
2023-05-30 01:05:39 +0300

    add base network class

---
+++ Currency App/Data/Network/HomeNetwork.swift
@@ -0,0 +1,48 @@
+//
+//  HomeNetwork.swift
+//  Currency App
+//
+//  Created by Mohamed Khaled on 26/05/2023.
+//
+
+import Foundation
+import RxSwift
+
+protocol APIService {
+    func fetch(urlRequest: URLRequest) -> Observable<Data>
+}
+
+class NetworkService: APIService {
+    
+    func fetch(urlRequest: URLRequest) -> Observable<Data> {
+        return Observable.create { observer in
+            
+            // Check for internet connectivity
+            guard Reachability.isConnectedToNetwork() else {
+                observer.onError(NetworkError.noInternet)
+                return Disposables.create()
+            }
+            
+            let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+                if let error = error {
+                    observer.onError(error)
+                    return
+                }
+                
+                guard let responseData = data else {
+                    observer.onError(NetworkError.noData)
+                    return
+                }
+                
+                observer.onNext(responseData)
+                observer.onCompleted()
+            }
+            
+            task.resume()
+            
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+    }
+}
