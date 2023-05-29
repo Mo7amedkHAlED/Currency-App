77904df4c78c49c1fb847cff6c39fc667532baec
Mohamed Khaled <mohammdk393@gmail.com>
2023-05-30 01:02:03 +0300

    add constants enum that holds all app constants

---
+++ Currency App/Constant/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Currency App
+//
+//  Created by Mohamed Khaled on 26/05/2023.
+//
+
+import Foundation
+
+enum Constants {
+    static let fixerBaseURL = "http://data.fixer.io/api/"
+    static let fixorPath: String = "/latest"
+    static let fixerAPIKey = "hq7wRn4A55WNOOkwXSa5OL7cuLXzP9BH"
+    static let access_key = "6114932191983c45c1deed4a829428b3"
+    static let access_keyName = "access_key"
+    static let format: String = "format"
+    
+}
