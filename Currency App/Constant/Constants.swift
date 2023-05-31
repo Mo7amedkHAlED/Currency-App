25e56436d8c5f739998e82b1f2bb0d34da14685a
Mohamed Khaled <mohammdk393@gmail.com>
2023-05-31 03:09:29 +0300

    update access key and remove unused api key

--- Currency App/Constant/Constants.swift
+++ Currency App/Constant/Constants.swift
@@ -10,8 +10,7 @@ import Foundation
 enum Constants {
     static let fixerBaseURL = "http://data.fixer.io/api/"
     static let fixorPath: String = "/latest"
-    static let fixerAPIKey = "hq7wRn4A55WNOOkwXSa5OL7cuLXzP9BH"
-    static let access_key = "6114932191983c45c1deed4a829428b3"
+    static let access_key = "b5649fa280a20166c0d648394f986e73"
     static let access_keyName = "access_key"
     static let format: String = "format"
     
