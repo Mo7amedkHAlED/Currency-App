2124e950d14192074cb3ec318b7863423c835e51
Mohamed Khaled <mohammdk393@gmail.com>
2023-05-31 03:10:26 +0300

    add PopularCurrencies model

---
+++ Currency App/Domain/Entities/PopularCurrencies.swift
@@ -0,0 +1,13 @@
+//
+//  PopularCurrencies.swift
+//  Currency App
+//
+//  Created by Mohamed Khaled on 31/05/2023.
+//
+
+import Foundation
+
+struct PopularCurrencies {
+    let currency: String
+    let rate: Float
+}
